93dd10f045ce764a6f8fef86514d7f5b76dcad6b
Andy Revell <andyrevell93@gmail.com>
2021-03-12 19:45:58 -0500

    update

--- papers/brainAtlas/brainAtlasAnalysis.py
+++ papers/brainAtlas/brainAtlasAnalysis.py
@@ -23,15 +23,18 @@ Example:
 #%%
 import sys
 import os
+import json
 import pkg_resources
 from os.path import join 
 from revellLab.packages.imaging.randomAtlas import randomAtlasGeneration as RAG
+from revellLab.packages.imaging.regionMorphology import regionMophology
 
 #%% Paths and File names
 tools = pkg_resources.resource_filename("revellLab", "tools")
 
+atlasfilesPath = join(tools, "atlases", "atlasMetadata.json")
 MNItemplateBrainPath = join( tools, "mniTemplate", "mni_icbm152_t1_tal_nlin_asym_09c_182x218x182_brain.nii.gz")
-randomAtlasesPath = join(tools, "randomAtlasesWholeBrainMNI")
+randomAtlasesPath = join(tools, "atlases", "randomAtlasesWholeBrainMNI")
 
 #%% Paramters
 
@@ -40,10 +43,20 @@ randomAtlasRegions, permutations = [10, 30, 50, 75, 100, 200, 300, 400, 500, 750
 
 
 
+#%% Read data
+
+with open(atlasfilesPath) as f: atlasfiles = json.load(f)
 
 
-#%% 01 Random Atlases
+
+#%% 01 Atlases
+
+#generate random atlases
 RAG.batchGenerateRandomAtlases(randomAtlasRegions, permutations, MNItemplateBrainPath, randomAtlasesPath)
+
+#measure volumes and sphericities
+
+
 
 
 
