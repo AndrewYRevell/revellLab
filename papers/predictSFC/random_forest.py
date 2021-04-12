935cbffd4354e58d2d66df4986a367d4b5bdd2d5
Lena Armstrong <56137845+lena318@users.noreply.github.com>
2021-04-12 13:49:40 -0400

    Update random_forest.py
    Updates to random forest for data classes

--- papers/predictSFC/random_forest.py
+++ papers/predictSFC/random_forest.py
@@ -23,7 +23,7 @@ import numpy as np
 from python_files.testing_python_toolbox import electrode_row_and_column_names
 
 
-def FC_SC_random_forest(features, FC_preictal_list, FC_ictal_list, FC_list, electrode_localization_by_atlas):
+def FC_SC_random_forest(features, FC_preictal_list, FC_ictal_list, FC_list):
     # average preictal
     FC_average_preictal = np.zeros(shape=(len(FC_preictal_list), len(FC_preictal_list[0]), len(FC_preictal_list[0][0])))
     print(FC_average_preictal.shape)
@@ -47,66 +47,6 @@ def FC_SC_random_forest(features, FC_preictal_list, FC_ictal_list, FC_list, elec
 
     for L in range(len(FC_ictal_list)):
         FC_list[L] = FC_ictal_list[L] - FC_preictal_list[L]
-
-    # Remove electrodes in "electrode localization" not found in Functional Connectivity matrices
-    electrode_localization_names = []
-    for file in range(len(electrode_localization_by_atlas)):
-        electrode_localization_names.append(np.array(electrode_localization_by_atlas[file]['electrode_name']))
-        electrode_localization_by_atlas[file] = electrode_localization_by_atlas[file][
-            np.in1d(electrode_localization_names[file], electrode_row_and_column_names)]
-
-    # Remove electrodes in the Functional Connectivity matrices not found in "electrode localization"
-    for file in range(len(electrode_localization_names)):
-        not_in_functional_connectivity = np.in1d(electrode_row_and_column_names, electrode_localization_names)
-        for i in range(len(FC_list)):
-            for j in range(len(FC_list[i])):
-                FC_list[file][i] = FC_list[file][i][not_in_functional_connectivity, :, :]
-                FC_list[file][i] = FC_list[file][i][:, not_in_functional_connectivity, :]
-
-    # Fisher z-transform of functional connectivity data. This is to take means of correlations and do correlations to
-    # the structural connectivity
-    for file in range(len(FC_list)):
-        for i in range(len(FC_list[file])):
-            FC_list[file][i] = np.arctanh(FC_list[file][i])
-
-    # Remove structural ROIs not in electrode_localization ROIs
-    electrode_ROIs = []
-    structural_index = []
-    for file in range(len(electrode_localization_by_atlas)):
-        electrode_ROIs.append(np.unique(np.array(electrode_localization_by_atlas[file].iloc[:, 4])))
-        electrode_ROIs[file] = electrode_ROIs[file][~(electrode_ROIs[file] == 0)]  # remove region 0
-        structural_index.append(np.array(electrode_ROIs[file] - 1))  # subtract 1 because of python's zero indexing
-    for row in range(len(features)):
-        for col in range(len(features[row])):
-            features[row][col] = features[row][col][structural_index[row], :]
-            features[row][col] = features[row][col][:, structural_index[row]]
-
-    # Taking average functional connectivity for those electrodes in same atlas regions
-    ROIs = []
-    for file in range(len(FC_list)):
-        print('length FC_List', len(FC_list))
-        for i in range(len(FC_list[file])):
-            print('length i FC_List', len(FC_list[file]))
-            ROIs.append(np.array(electrode_localization_by_atlas[file].iloc[:, 4]))
-            for r in range(len(electrode_ROIs[file])):
-                index_logical = (ROIs[file] == electrode_ROIs[file][r])
-                index_first = np.where(index_logical)[0][0]
-                index_second_to_end = np.where(index_logical)[0][1:]
-                mean = np.mean(FC_list[file][i][index_logical, :, :], axis=0)
-                # Fill in with mean.
-                FC_list[file][i][index_first, :, :] = mean
-                FC_list[file][i][:, index_first, :] = mean
-                # delete the other rows and columns belonging to same region.
-                FC_list[file][i] = np.delete(FC_list[file][i], index_second_to_end, axis=0)
-                FC_list[file][i] = np.delete(FC_list[file][i], index_second_to_end, axis=1)
-                # keeping track of which electrode labels correspond to which rows and columns
-                ROIs[file] = np.delete(ROIs[file], index_second_to_end, axis=0)
-            # remove electrodes in the ROI labels as zero
-            index_logical = (ROIs[file] == 0)
-            index = np.where(index_logical)[0]
-            FC_list[file][i] = np.delete(FC_list[file][i], index, axis=0)
-            FC_list[file][i] = np.delete(FC_list[file][i], index, axis=1)
-            ROIs[file] = np.delete(ROIs, index, axis=0)
 
     # break down feature matrix 2D - 1D FC matrix !!!
     features_2D = []
