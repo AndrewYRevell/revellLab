9e3382ea05bfab372c5dff48f85252830488f243
Lena Armstrong <56137845+lena318@users.noreply.github.com>
2021-05-11 14:23:45 -0400

    Update random_forest_script.py

--- papers/predictSFC/random_forest_script.py
+++ papers/predictSFC/random_forest_script.py
@@ -23,27 +23,29 @@ Output: Random Forest predictions of FC based on SC
 """
 import os
 import pickle
+
+from pandas import np
+
 from python_files import random_forest
-from revellLab.packages.dataClass.DataClassSfc import DataClassSfc
+from revellLab.packages.dataClass import DataClassSfc
 
 # Each structural connectivity matrix gets a label of a functional connectivity matrix,
 # which is done multiple times if there were multiple seizures
 
-file_directory = '/Users/larmstrong2020/mount/DATA/Human_Data/BIDS_processed/'
+file_directory = '/Users/larmstrong2020/mount/DATA/Human_Data/BIDS_processed/papers/paper005/data/data_processed/aggregated_data'
 
 # get data into data class
-sfc_datapath = os.path.join("/media","arevell","sharedSSD","linux","papers","paper005", "data", "data_processed", "aggregated_data")
+# sfc_datapath = os.path.join("/media","arevell","sharedSSD","linux","papers","paper005", "data", "data_processed", "aggregated_data")
 RID = "RID0278"
 iEEG_filename = "HUP138_phaseII"
 
 start_time_usec = 416023190000
 stop_time_usec = 416112890000
-fname = os.path.join(sfc_datapath, f"sub-{RID}_{iEEG_filename}_{start_time_usec}_{stop_time_usec}_data.pickle")
-
-os.path.exists(fname)
+fname = os.path.join(file_directory, f"sub-{RID}_{iEEG_filename}_{start_time_usec}_{stop_time_usec}_data.pickle")
 
 if (os.path.exists(fname)):
-    with open(fname, 'rb') as f: all_data = pickle.load(f)
+    with open(fname, 'rb') as f:
+        all_data = pickle.load(f)
 
 sfc_data = DataClassSfc.sfc(**all_data)
 atlas = sfc_data.get_atlas_names()[1]
@@ -54,13 +56,14 @@ st = int((secondsBeforeSpread-10)/skipWindow)
 stp = int((secondsBeforeSpread +20)/skipWindow)
 
 SC, SC_regions = sfc_data.get_structure(atlas)
+# Get electrode localization
 electrodeLocalization = sfc_data.get_electrodeLocalization(atlas)
 
-# Get electrode localization
-sfc_data_electrode_localized = sfc_data.get_electrodeLocalization()
+# remove any electrodes in spread with no localization data
+sfc_data_electrode_localized = np.intersect1d(SC_regions, electrodeLocalization["electrode_name"], return_indices=True)
 
 # Creates features, which are structural connectivity matrices
-structural_features = sfc_data.create_feature_matrix()
+structural_features = sfc_data_electrode_localized.create_feature_matrix()
 
 # Accesses files for functional connectivity matrices (preictal)
 FC_preictal_file_path_array = []
